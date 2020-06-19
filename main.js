9d6f8bee9eea557c9c5a77591f0a954c4ca4c74e
tkddn897 <57991980+tkddn897@users.noreply.github.com>
2020-06-19 17:20:23 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -13,3 +13,63 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  console.log(event.target.dataset.link);
+  const scrollTO = document.querySelector(link);
+  scrollTO.scrollIntoView({ behavior: "smooth" });
+});
+
+// const about = document.querySelector("#about");
+
+// about.addEventListener("click", () => {
+//   console.log(about);
+//
+// });
+// switch (link) {
+//     case "#about":
+//       window.scroll({
+//         top: 580,
+//         left: 0,
+//         behavior: "smooth",
+//       });
+//       break;
+//     case "#skills":
+//       window.scroll({
+//         top: 1356,
+//         left: 0,
+//         behavior: "smooth",
+//       });
+//       break;
+//     case "#work":
+//       window.scroll({
+//         top: 1948,
+//         left: 0,
+//         behavior: "smooth",
+//       });
+//       break;
+//     case "#testimonial":
+//       window.scroll({
+//         top: 2592,
+//         left: 0,
+//         behavior: "smooth",
+//       });
+//       break;
+//     case "#contact":
+//       window.scroll({
+//         top: 3118,
+//         left: 0,
+//         behavior: "smooth",
+//       });
+//       break;
+//     default:
+//       break;
+//   }
